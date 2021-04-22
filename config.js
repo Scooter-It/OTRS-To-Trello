ec1eb1ff0e0d30f5d8d8edde6549c0a5fe3bb93a
Scott <scott@scotts.cc>
2021-04-22 15:09:30 +0100

    Add old codebase

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trelloKey : "trello-key",
+    trelloToken: "trello-token",
+    trelloOrganizationName: "trello-organization",
+    otrsBaseAddress: "https://otrs.example.com/otrs/",
+    otrsUser: "otrs-login",
+    otrsPassword: "otrs-password",
+}
